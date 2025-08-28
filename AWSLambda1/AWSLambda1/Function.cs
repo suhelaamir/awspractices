92474c958e360c86fc87eadfab84836468bc9f6a
Mohammad Amir Suhel <suhelaamir68@gmail.com>
2025-08-28 15:05:35 +0530

    Integrated API gateway and handle the null response.

--- AWSLambda1/AWSLambda1/Function.cs
+++ AWSLambda1/AWSLambda1/Function.cs
@@ -2,6 +2,7 @@ using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using System.Text.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -17,7 +18,7 @@ public class Function
     /// <param name="input">The event for the Lambda function handler to process.</param>
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
-    public async Task<User?> FunctionHandler(
+    public async Task<APIGatewayHttpApiV2ProxyResponse?> FunctionHandler(
         APIGatewayHttpApiV2ProxyRequest request, 
         ILambdaContext context
         )
@@ -31,8 +32,20 @@ public class Function
         var dynamoDBContext = new DynamoDBContext(client);
 
         var user = await dynamoDBContext.LoadAsync<User>(input);
+        if (user != null)
+        {
+            return new APIGatewayHttpApiV2ProxyResponse() 
+            {
+                Body=JsonSerializer.Serialize(user),
+                StatusCode = 200
+            };
+        }
 
-        return user;
+        return new APIGatewayHttpApiV2ProxyResponse()
+        {
+            Body= "Invalid userId in path",
+            StatusCode = 404
+        };
     }
 }
 public class User
