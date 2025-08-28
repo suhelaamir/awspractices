17a0a05d3b50f631c631cd61f15e1e2e991626a3
Mohammad Amir Suhel <suhelaamir68@gmail.com>
2025-08-28 14:57:03 +0530

    Integrated the API Gateway to the lambda function

--- AWSLambda1/AWSLambda1/Function.cs
+++ AWSLambda1/AWSLambda1/Function.cs
@@ -1,5 +1,6 @@
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -16,16 +17,16 @@ public class Function
     /// <param name="input">The event for the Lambda function handler to process.</param>
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
-    public async Task<User?> FunctionHandler(Int32 input, ILambdaContext context)
+    public async Task<User?> FunctionHandler(
+        APIGatewayHttpApiV2ProxyRequest request, 
+        ILambdaContext context
+        )
     {
-        //return input?.Name?.ToUpper();
-        //var dynamoDBContext = new DynamoDBContext(new AmazonDynamoDBClient());
-
-        //// Load the User item with the given Id (input)
-        //var user = await dynamoDBContext.LoadAsync<User>(input);
-
-        //return user;
-        // Use the recommended constructor for DynamoDBContext
+        request.PathParameters.TryGetValue("userId", out var userId);
+        if (userId == null || !int.TryParse(userId, out var input))
+        {
+            return null;
+        }
         using var client = new AmazonDynamoDBClient();
         var dynamoDBContext = new DynamoDBContext(client);
 
