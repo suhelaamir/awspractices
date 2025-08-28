65f7fba555a80eaadb3bd28bfc28a8b091b97226
Mohammad Amir Suhel <suhelaamir68@gmail.com>
2025-08-28 16:15:08 +0530

    We have used IDynamoDBContext instead DynamoDBContext for dependency resolution.

--- AWSLambda1/AWSLambda1/Function.cs
+++ AWSLambda1/AWSLambda1/Function.cs
@@ -11,7 +11,12 @@ namespace AWSLambda1;
 
 public class Function
 {
-    
+    private readonly IDynamoDBContext _dynamoDBContext;
+
+    public Function()
+    {
+        _dynamoDBContext = new DynamoDBContext(new AmazonDynamoDBClient());
+    }
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
@@ -23,29 +28,59 @@ public class Function
         ILambdaContext context
         )
     {
+        return request.RequestContext.Http.Method.ToUpper() switch
+        {
+            "GET" => await HandleGetRequest(request),
+            "POST" => await HandlePostRequest(request),
+        };
+    }
+
+    private async Task<APIGatewayHttpApiV2ProxyResponse?> HandlePostRequest(APIGatewayHttpApiV2ProxyRequest request)
+    {
+        var user = JsonSerializer.Deserialize<User>(request.Body);// ?? string.Empty);
+        if (user == null)
+        {
+            return BadRequest("Invalid user in body");
+        }
+
+        await _dynamoDBContext.SaveAsync(user);
+        return new APIGatewayHttpApiV2ProxyResponse()
+        {
+            Body = JsonSerializer.Serialize(user),
+            StatusCode = 200
+        };
+    }
+
+    private static APIGatewayHttpApiV2ProxyResponse BadRequest(string message)
+    {
+        return new APIGatewayHttpApiV2ProxyResponse()
+        {
+            Body = message,
+            StatusCode = 400
+        };
+    }
+
+    private async Task<APIGatewayHttpApiV2ProxyResponse?> HandleGetRequest(APIGatewayHttpApiV2ProxyRequest request)
+    {
         request.PathParameters.TryGetValue("userId", out var userId);
         if (userId == null || !int.TryParse(userId, out var input))
         {
             return null;
         }
-        using var client = new AmazonDynamoDBClient();
-        var dynamoDBContext = new DynamoDBContext(client);
+        //using var client = new AmazonDynamoDBClient();
+        
 
-        var user = await dynamoDBContext.LoadAsync<User>(input);
+        var user = await _dynamoDBContext.LoadAsync<User>(input);
         if (user != null)
         {
-            return new APIGatewayHttpApiV2ProxyResponse() 
+            return new APIGatewayHttpApiV2ProxyResponse()
             {
-                Body=JsonSerializer.Serialize(user),
+                Body = JsonSerializer.Serialize(user),
                 StatusCode = 200
             };
         }
 
-        return new APIGatewayHttpApiV2ProxyResponse()
-        {
-            Body= "Invalid userId in path",
-            StatusCode = 404
-        };
+        return BadRequest("Invalid user in body");
     }
 }
 public class User
